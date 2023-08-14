336fcdc4dd20bcf9165d05d0a26a2ad9d036c36d
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-08-13 23:05:32 -0300

    css added

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -5,11 +5,17 @@ import axios from "axios";
 function App() {
   return (
     <div className="App">
+      <section className="pesquisa">
+        <input type="text" placeholder="Digite seu local" />
+        <button>Pesquisar</button>
+      </section>
       <section className="body">
-        <div>New York</div>
-        <div>20º</div>
-        <div>Cloudy</div>
-        <div>20/12</div>
+        <div className="content">New York</div>
+        <div className="content">20ºC</div>
+      </section>
+      <section className="footer">
+        <div className="content">Cloudy</div>
+        <div className="content">20/12</div>
       </section>
     </div>
   );
