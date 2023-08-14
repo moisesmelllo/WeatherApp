b1f7b5425e4d5509e2717481e128df77c25fa7a6
Moises Melo Viana Ferreira <125166797+moisesmelllo@users.noreply.github.com>
2023-08-14 01:09:24 +0000

    adicionando css

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
+import "./index.css";
 import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <section className="body">
+        <div>New York</div>
+        <div>20º</div>
+        <div>Cloudy</div>
+        <div>20/12</div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
