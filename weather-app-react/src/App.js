90a1fc21423acb9037fd24dde8acba889fa0846e
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-08-14 23:20:51 -0300

    axios api

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import axios from "axios";
+import { key } from "./keys";
+import {format} from 'date-fns'
 
 function App() {
+  const [apiData, setApiData] = useState({});
+
+
+  useEffect(() => {
+    const api_key = `https://api.openweathermap.org/data/2.5/weather?q=guarulhos&APPID=${key}&units=metric`
+    console.log(api_key)
+
+  axios.get(api_key)
+    .then(response => {
+      setApiData(response.data)
+    })
+    .catch(error => {
+      console.error('Erro na requisição: ', error);
+    });
+  }, [])
+
   return (
     <div className="App">
       <section className="pesquisa">
         <input type="text" placeholder="Digite seu local" />
         <button>Pesquisar</button>
       </section>
+      {Object.keys(apiData).length > 0 && (
+        <>
       <section className="body">
-        <div className="content">New York</div>
-        <div className="content">20ºC</div>
+        <div className="content">{apiData.name}</div>
+        <div className="content">{apiData.main.temp}ºC</div>
       </section>
       <section className="footer">
-        <div className="content">Cloudy</div>
-        <div className="content">20/12</div>
+        <div className="content">{apiData.weather[0].main}</div>
+        <div className="content">{format(new Date(), 'dd/MM')}</div>
       </section>
+      </>
+        )}
     </div>
   );
 }
