632ed7de05ab60134d9ec0d9f39f7e75aaac8fde
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-08-16 21:04:27 -0300

    fix bugs

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -7,9 +7,11 @@ import {format} from 'date-fns'
 function App() {
   const [apiData, setApiData] = useState({});
   const [searchLocation, setSearchLocation] = useState("");
+  const [apiError, setApiError] = useState(null);
 
   const fetchData = () => {
     const api_key = `https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&APPID=${key}&units=metric`
+    console.log(api_key);
 
     axios.get(api_key)
       .then(response => {
@@ -17,37 +19,54 @@ function App() {
       })
       .catch(error => {
         console.error('Erro na requisição: ', error);
+        setApiError("Erro na requisição.");
       });
+
+      
   };
 
-  const handleSearch = () => {
-    fetchData();
-  };
+  const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+          fetchData();
+        }}
 
-
-  return (
+return (
     <div className="App">
       <section className="pesquisa">
         <input 
-        type="text" 
-        placeholder="Digite seu local" 
-        value={searchLocation}
-        onChange={(e) => setSearchLocation(e.target.value)}
+          className="input"
+          type="text" 
+          placeholder="Digite seu local" 
+          value={searchLocation}
+          onChange={(e) => setSearchLocation(e.target.value)}
+          onKeyUp={handleKeyPress}
         />
-        <button onClick={() => handleSearch()}>Pesquisar</button>
+        <button onClick={() => fetchData()}>Pesquisar</button>
       </section>
-      {Object.keys(apiData).length > 0 && (
+      {apiError && <div className="error">{apiError}</div>}
+      {apiData.name && (
         <>
-      <section className="body">
-        <div className="content">{apiData.name}</div>
-        <div className="content">{apiData.main.temp}ºC</div>
-      </section>
-      <section className="footer">
-        <div className="content">{apiData.weather[0].main}</div>
-        <div className="content">{format(new Date(), 'dd/MM')}</div>
-      </section>
-      </>
-        )}
+          <section className="body">
+            <div className="content">{apiData.name}</div>
+            {apiData.main && (
+              <div className="content">{apiData.main.temp.toFixed(0)}ºC</div>
+            )}
+          </section>
+          <section className="media">
+            {apiData.sys && (
+              <img src={`https://flagsapi.com/${apiData.sys.country}/shiny/64.png`} alt="Flag" />
+            )}
+          </section>
+          <section className="footer">
+            {apiData.weather && apiData.weather[0] && (
+              <>
+                <div className="content">{apiData.weather[0].main}</div>
+                <div className="content">{format(new Date(), 'dd/MM')}</div>
+              </>
+            )}
+          </section>
+        </>
+      )}
     </div>
   );
 }
