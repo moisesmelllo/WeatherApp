391a59d71d69238f26f06515d341297f35d163a9
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-08-18 22:58:35 -0300

    finalizando

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -14,9 +14,12 @@ function App() {
     const api_key = `https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&APPID=${key}&units=metric`
     const api_img = `https://api.unsplash.com/search/photos?query=${searchLocation}&client_id=${img_key}`
 
+    
+
     axios.get(api_key)
       .then(response => {
         setApiData(response.data)
+        console.log(response);
         setApiError(null)
         setSearchLocation("")
         fetchImage(api_img)
@@ -48,7 +51,8 @@ function App() {
 return (
     <div className={`App ${ Object.keys(apiData).length ? 'background' : 'default-background'}`}
        style={{ backgroundImage:  Object.keys(apiData).length && `url(${imageUrl})` }}>
-      
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       <section className="pesquisa">
         <input 
           className="input"
@@ -60,16 +64,19 @@ return (
         />
         <button onClick={() => fetchData()}>Pesquisar</button>
       </section>
-      <section className="box">
+      
       {apiData.name && (
+        <section className="box">
         <>
           <section className="body">
             <div className="container">
               <div className="content">{apiData.name}</div>
-              {apiData.main && (
-                <div className="content degree">{apiData.main.temp.toFixed(0)}</div>
-              )}
-              <p className="metric">ºC</p>
+              <div className="temperatura">
+                {apiData.main && (
+                  <div className="content degree">{apiData.main.temp.toFixed(0)}</div>
+                )}
+                <p className="metric">ºC</p>
+              </div>
             </div>
           </section>
           <section className="media">
@@ -77,18 +84,33 @@ return (
               <img src={`https://flagsapi.com/${apiData.sys.country}/shiny/64.png`} alt="Flag" />
             )}
           </section>
+          <section className="extra-info-container">
+            <div className="vento">
+              <p><span className="material-symbols-outlined">
+                air
+                </span></p>
+              <p>{apiData.wind.speed}</p>
+            </div>
+            <div className="umidade">
+              <p><span className="material-symbols-outlined umidade-simbol">
+                humidity_low
+                </span></p>
+              <p>{apiData.main.humidity}</p>
+            </div>
+          </section>
           <section className="footer">
             {apiData.weather && apiData.weather[0] && (
               <>
                 <div className="content">{apiData.weather[0].main}</div>
-                <div className="content">{format(new Date(), 'dd/MM')}</div>
+                <div className="content-date">{format(new Date(), 'dd/MM')}</div>
               </>
             )}
           </section>
         </>
+        </section>
       )}
         {apiError && <div className="error-content">{apiError}</div>}
-      </section>
+      
     </div>
   );
 }
